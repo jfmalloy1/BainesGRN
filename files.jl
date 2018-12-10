f39bf317cc69199977df01738607596fc4512d32
John <jfmalloy1@gmail.com>
2018-12-10 10:06:44 -0700

    Changed .txt files to .csv, created environment

--- files.jl
+++ files.jl
@@ -1,4 +1,5 @@
 #Creates a random sequence of ABCD of length chars
+#LIMITATIONS: only goes A-D currently
 function rand_seq(chars)
   seq = ""
   for i = 1:chars
@@ -17,27 +18,41 @@ function rand_seq(chars)
 end
 
 #Writes the genotypes for each bug
-function genotypes()
-  n = 4 #number of initial characters
-
+function genotypes(init_chars, coding_len)
   #Open and writes 4 bug files
   for m in 1:4
-    #filename = "bug" * n * ".txt"
-    #println(filename)
-    f = open("bug$m.txt", "w")
+    f = open("bug$m.csv", "w")
     for i in 1:100
       genes = ""
       for j in 1:18
-        genes = genes*rand_seq(n)*","
+        genes = genes*rand_seq(init_chars)*","
       end
-      println(f, genes*rand_seq(5))
+      println(f, genes*rand_seq(coding_len))
     end
     close(f)
   end
 end
 
+function environment(coding_len)
+  #15 environments - 6 input, 100 have to have, 100 must not have
+  #all length coding_len
+  f = open("environment.csv", "w")
+  for i in 1:15
+    env = ""
+    for i in 1:105
+      env = env*rand_seq(coding_len)*","
+    end
+    println(f, env*rand_seq(coding_len))
+  end
+  close(f)
+end
+
 function main()
-  genotypes()
+  init_chars = 4 #number of characters in each control sequence
+  coding_len = 5 #length of each coding DNA (and environmental length)
+  genotypes(init_chars, coding_len)
+
+  environment(coding_len)
 end
 
 main()
